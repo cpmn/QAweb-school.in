ccf3914d81c1945a1d65bbb38d6dff12944cf930
unknown <Virginia Sanabria@VSANABRIAM-DH02.jalasoft.local>
2016-07-11 14:48:16 -0400

    update screenshot path

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,7 +7,7 @@ Date: 06 Jul 2016
 After do |scenario|
   if(scenario.failed?)
     screenshot_name = scenario.__id__.to_s + ".png"  
-    page.driver.browser.save_screenshot("/#{screenshot_name}") 
+    page.driver.browser.save_screenshot("screenshots/#{screenshot_name}") 
     embed("screenshots/#{screenshot_name}", "image/png","Screenshots - Failed")
   end
 end
